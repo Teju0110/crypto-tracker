6c4126b08acbe3f96e21ffdeab6e9a46ce197ec4
tejasvisawant1998@gmail.com <tejasvisawant1998@gmail.com>
2022-11-23 12:18:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import CryptoCurrencies from "./Components/CryptoCurrencies";
+import CoinInfo from "./Pages/CoinInfo";
+import SignUp from "./Pages/SignUp";
+import Plan from "./Pages/Plan";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/plan" element={<Plan/>}/>
+          <Route path="/cryptoTracker" element={<CryptoCurrencies />} />
+          <Route path="/coins/:id" element={<CoinInfo />} exact />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
